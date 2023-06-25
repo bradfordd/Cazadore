a87456eb288128186ef92576ac2b904724de4497
bradfordd <dbradford1337@gmail.com>
2023-06-24 20:16:28 -0400

    search bar functionality added pt 2

--- src/BugReportList.jsx
+++ src/BugReportList.jsx
@@ -6,16 +6,21 @@ function BugReportList() {
   const [page, setPage] = useState(1); // Initial page
   const [perPage, setPerPage] = useState(10); // Items per page
   const [totalPages, setTotalPages] = useState(0); // Total pages
+  const [searchTerm, setSearchTerm] = useState(""); // Search term
 
   useEffect(() => {
     const fetchBugReports = async () => {
-      const data = await BugReportService.getAllBugReports(page, perPage);
+      const data = await BugReportService.getAllBugReports(
+        page,
+        perPage,
+        searchTerm
+      );
       setBugReports(data.bugReports);
       setTotalPages(Math.ceil(data.total / perPage)); // Calculate total pages
     };
 
     fetchBugReports();
-  }, [page, perPage]);
+  }, [page, perPage, searchTerm]); // Add searchTerm to dependencies
 
   const nextPage = () => {
     setPage(page + 1);
@@ -27,12 +32,23 @@ function BugReportList() {
     }
   };
 
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+    setPage(1); // Reset page number to 1 when search term changes
+  };
+
   return (
     <div>
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleSearchChange}
+        placeholder="Search by title..."
+      />{" "}
+      {/* Search input */}
       <div>
         Page: {page} / {totalPages}
-      </div>{" "}
-      {/* Display current page and total pages */}
+      </div>
       {bugReports.map((report) => (
         <div key={report._id}>
           <h2>{report.title}</h2>
