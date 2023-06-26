2c8b0103e74a8afe2d420bb785fc960e394bb987
bradfordd <dbradford1337@gmail.com>
2023-06-26 05:51:28 -0400

    added more detailed bug report upon selection

--- src/BugReportDetail.jsx
+++ src/BugReportDetail.jsx
@@ -5,6 +5,18 @@ function BugReportDetail({ bug, goBack }) {
     <div>
       <h1>{bug.title}</h1>
       <p>{bug.description}</p>
+      <p>Steps to Reproduce: {bug.stepsToReproduce}</p>
+      <p>Expected Result: {bug.expectedResult}</p>
+      <p>Actual Result: {bug.actualResult}</p>
+      <p>Priority: {bug.priority}</p>
+      <p>Status: {bug.status}</p>
+      <p>
+        Created by: {bug.createdBy.name} (Display name depending on your User
+        model structure)
+      </p>
+      <p>
+        Assigned to: {bug.assignedTo ? bug.assignedTo.name : "Not Assigned"}
+      </p>
       <button onClick={goBack}>Back to List</button>
     </div>
   );
